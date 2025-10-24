a7b73841da83110f33154d7e8a5621da9e1e775e
muuo <anthonymuuo92@gmail.com>
2025-10-24 14:15:07 +0300

    validation with zod and resolver in the form

--- src/app/checkout/personal.tsx
+++ src/app/checkout/personal.tsx
@@ -10,13 +10,29 @@ import {
   Controller,
   FormProvider,
 } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+
+const PersonalInformationSchema = z.object({
+  fullName: z
+    .string({ message: "fullName is required" })
+    .min(3, { message: "fullName must be longer that 3 characters" }),
+  address: z.string().min(1, { message: "please provide your Address" }),
+  city: z.string().min(1, { message: "City is required!" }),
+  postcode: z.string().min(1, { message: "Postal code is required!" }),
+  phone: z.string().min(1, { message: "Phone is required!" }),
+});
+
+type PersonalInfo = z.infer<typeof PersonalInformationSchema>;
 
 const PersonalDetailsForm = () => {
-  const form = useForm();
+  const form = useForm<PersonalInfo>({
+    resolver: zodResolver(PersonalInformationSchema),
+  });
 
   console.log("errors from forms", form.formState.errors);
 
-  const onNext: SubmitHandler<any> = (data) => {
+  const onNext: SubmitHandler<PersonalInfo> = (data) => {
     //need to validate the form
     console.log("this is the data", data);
     router.push("/checkout/payment");
@@ -42,14 +58,14 @@ const PersonalDetailsForm = () => {
           />
           <CustomTextInput
             label="Post Code"
-            name="postCode"
+            name="postcode"
             placeholder="134"
             containerStyle={{ flex: 1 }}
           />
         </View>
         <CustomTextInput
           label="Phone Number"
-          name="phoneNumber"
+          name="phone"
           placeholder="07123890033"
           inputMode="tel"
         />
